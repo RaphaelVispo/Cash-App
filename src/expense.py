2c30862d2aa3eeccee8217f72680c31d8d5898be
angeloslvrs <aesoliveres@up.edu.ph>
2023-06-09 02:13:56 +0800

    feat: generated reports

--- src/expense.py
+++ src/expense.py
@@ -93,3 +93,76 @@ def get_total_expenses(user):
             ''', header)
 
     print_table(table, header, False)
+    
+    
+def get_expenses_in_a_month(user,month):
+    header = ["Group Name", "Date", "Amount", "Settled?"]
+    table = get_table(
+        f'''
+    select group_name, expense_date, amount, case when is_settled = 0 then 'No' else 'Yes' end from USER_HAS_GROUP_EXPENSE a natural join HAS_GROUP natural join EXPENSE e join USER
+        u on e.creditor = u.user_id where MONTHNAME(expense_date) = \'{month}\' and a.user_id = \'{user}\';
+            ''', header)
+    
+    if table.empty == False:
+        print_table(table, header)
+    else:
+        print('No expenses for this month')
+        
+
+def get_expenses_with_a_friend(user, friend):
+    
+    header = ["User Name", "Date", "Amount", "Group Name", "Friend"]
+    
+    table = get_table (
+        f'''
+    SELECT a.user_name, e.expense_date, e.amount, group_name, c.user_name as friend FROM USER a 
+    JOIN USER_FRIEND b ON a.user_id = b.user_id 
+    JOIN USER c ON b.friend = c.user_id 
+    JOIN USER_HAS_GROUP_EXPENSE d on a.user_id = d.user_id 
+    natural join HAS_GROUP 
+    natural join EXPENSE e where c.user_name = \'{friend}\' and a.user_id = \'{user}\' order by e.expense_date desc;
+        ''', header)
+    
+    if table.empty == False:
+        print_table(table, header)
+    else:
+        print('No expenses for this friend')
+        
+        
+def get_expenses_with_a_group(user, group):
+    
+    header = ["User Name", "Date", "Amount", "Group Name", "Friend"]
+    
+    table = get_table (
+        f'''
+    SELECT a.user_name, e.expense_date, e.amount, group_name, c.user_name as friend FROM USER a 
+    JOIN USER_FRIEND b ON a.user_id = b.user_id 
+    JOIN USER c ON b.friend = c.user_id 
+    JOIN USER_HAS_GROUP_EXPENSE d on a.user_id = d.user_id 
+    natural join HAS_GROUP 
+    natural join EXPENSE e where group_name = \'{group}\' and a.user_id = \'{user}\'order by e.expense_date desc;
+        ''', header)
+    
+    if table.empty == False:
+        print_table(table, header)
+    else:
+        print('No expenses for this group')
+        
+def get_friends_with_outstanding_bal(user):
+    
+    header = ["User Name", "Friend"]
+    
+    table = get_table (
+        f'''
+    SELECT distinct u.user_name AS user_name, f.user_name AS friend_name
+        FROM USER u
+        JOIN USER_FRIEND uf ON u.user_id = uf.user_id
+        JOIN USER f ON uf.friend = f.user_id
+        JOIN USER_HAS_GROUP_EXPENSE uge ON u.user_id = uge.user_id
+        JOIN EXPENSE e ON uge.expense_id = e.expense_id
+        WHERE e.is_settled = 0 and u.user_id = \'{user}\';
+        ''', header)
+    if table.empty == False:
+        print_table(table, header)
+    else:
+        print('No friends with outstanding balances')
--- src/group.py
+++ src/group.py
@@ -13,3 +13,22 @@ def get_groups(user):
             ''', header)
 
     print_table(table, header)
+
+def get_groups_with_outstanding_bal(user):
+    
+    header = ["User Name", "Group"]
+    
+    table = get_table (
+        f'''
+    SELECT u.user_name, g.group_name
+        FROM USER u
+        JOIN USER_HAS_GROUP_EXPENSE uge ON u.user_id = uge.user_id
+        JOIN EXPENSE e ON uge.expense_id = e.expense_id
+        JOIN HAS_GROUP g ON uge.group_id = g.group_id
+        WHERE e.is_settled = 0
+        AND u.user_id = \'{user}\';
+        ''', header)
+    if table.empty == False:
+        print_table(table, header)
+    else:
+        print('No groups with outstanding balances')
