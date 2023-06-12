d87b3bb1d5324c66c22fba8e65a7663dc99ded40
RaphaelVispo <rsvispo@up.edu.ph>
2023-06-12 11:50:20 +0800

    Feat: add pick user

--- src/user.py
+++ src/user.py
@@ -10,3 +10,21 @@ def get_username(user):
             ''', header)
 
     print_table(table, header, False)
+
+def pick_user():
+    print_msg_box("Choose user")
+    header = ["user_id", "name"]
+    table = get_table(
+        "SELECT * FROM USER ORDER BY RAND() LIMIT 5;",
+        header
+    )
+
+    clean_table = table.drop(columns='user_id', axis=1)
+    header.remove("user_id")    
+    print_table(clean_table, header)
+
+    c=choice(5)
+
+    get_username(table.user_id[c])
+    return dict(table.loc[c])
+ 
