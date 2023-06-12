650b348c6811e381a7e8ebde40a6023ab4bc98d4
sicnarfmgcls <135854996+sicnarfmgcls@users.noreply.github.com>
2023-06-12 21:15:59 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -12,25 +12,63 @@ name = picked["name"]
 print(f"user_id: {user} , name: {name}" )
 
 
-def choose_Efg():
+def add_Menu():
     global efg_Choice
     while True:
         try:
-            print("[1] EXPENSE")
+            print("[1] GROUP EXPENSE")
             print("[2] FRIEND")
-            print("[3] GROUP")
+            print("[3] EXPENSE TO A GROUP\n")
 
             efg_Choice = int(input("Your choice: "))
             if efg_Choice > 0 and efg_Choice < 4:
                 return efg_Choice
                 break;
             else:
-                print("Invalid entry. Please try again.")
+                print("Invalid entry. Please try again.\n")
         except:
             print("Invalid entry. Please try again.\n")
             continue
+
+
+def delete_Menu():
+    global efg_Choice
+    while True:
+        try:
+            print("[1] GROUP EXPENSE")
+            print("[2] FRIEND")
+            print("[3] EXPENSE FROM A GROUP\n")
+
+            efg_Choice = int(input("Your choice: "))
+            if efg_Choice > 0 and efg_Choice < 4:
+                return efg_Choice
+                break;
+            else:
+                print("Invalid entry. Please try again.\n")
+        except:
+            print("Invalid entry. Please try again.\n")
+            continue
+        
             
-            
+def search_Update_Menu():
+    global efg_Choice
+    while True:
+        try:
+            print("[1] EXPENSE")
+            print("[2] FRIEND")
+            print("[3] GROUP\n")
+
+            efg_Choice = int(input("Your choice: "))
+            if efg_Choice > 0 and efg_Choice < 4:
+                return efg_Choice
+                break;
+            else:
+                print("Invalid entry. Please try again.\n")
+        except:
+            print("Invalid entry. Please try again.\n")
+            continue
+        
+
 def choose_View():
     global view_Choice
     while True:
@@ -42,11 +80,10 @@ def choose_View():
             print("[4] CURRENT BALANCE FROM ALL EXPENSES")
             print("[5] ALL FRIENDS WITH OUTSTANDING BALANCE")
             print("[6] ALL GROUPS")
-            print("[7] ALL GROUPS WITH OUTSTANDING BALANCE")
-            print("What would you like to view?")
+            print("[7] ALL GROUPS WITH OUTSTANDING BALANCE\n")
 
             view_Choice = int(input("Your choice: "))
-            if view_Choice > 0 and view_Choice < 7:
+            if view_Choice > 0 and view_Choice < 8:
                 return view_Choice
                 break;
             else:
@@ -60,13 +97,14 @@ def main_Menu():
     global action
     while True:
         try:
+            print("What would you like to do?")
             print("[1] ADD")
             print("[2] DELETE")
             print("[3] SEARCH")
             print("[4] UPDATE")
             print("[5] VIEW")
             print("[0] EXIT CASH-APP\n")
-            action = int(input("What would you like to do? "))
+            action = int(input("Your choice: "))
         
             if action >= 0 and action <= 5:
                 return action
@@ -76,71 +114,68 @@ def main_Menu():
         except:
             print("Invalid entry. Please try again.\n")
             continue
-
-
-## instead of print, lagay yung function for it
+            
+            
 def action_Run():
     if action == 1:
         print("What would you like to add?\n")
-        choose_Efg()
+        add_Menu()
         if efg_Choice == 1:
-            print("Add expense")
-            add_expense(user)
-            print("Add friend")
+            add_user_group_expense(user):
+        elif efg_Choice == 2:
+            add_friend(user)
         elif efg_Choice == 3:
-            print("Add group")
+            add_user_expense(user)
     
     elif action == 2:
         print("What would you like to delete?\n")
-        choose_Efg()
+        delete_Menu()
         if efg_Choice == 1:
-            print("delete expense")
-            delete_expense(user)
+            delete_user_group_expense(user):
         elif efg_Choice == 2:
-            print("delete friend")
+            delete_friend(user)
         elif efg_Choice == 3:
-            print("delete group")
+            delete_user_choice(user)
     
     elif action == 3:
         print("What would you like to search?\n")
-        choose_Efg()
+        search_Update_Menu()
         if efg_Choice == 1:
-            print("search expense")
+            search_expense(user)
         elif efg_Choice == 2:
-            print("search friend")
+            search_friend(user)
         elif efg_Choice == 3:
-            print("search group")
+            search_group(user)
     
     elif action == 4:
         print("What would you like to update?\n")
-        choose_Efg()
+        search_Update_Menu()
         if efg_Choice == 1:
-            print("update expense")
+            edit_expense(user)
         elif efg_Choice == 2:
-            print("update friend")
+            edit_friend(user)
         elif efg_Choice == 3:
-            print("update group")
+            edit_group(user)
     
     elif action == 5:
         choose_View()
         if view_Choice == 1:
-            print("expense month")
+            get_expenses_in_a_month(user)
         elif view_Choice == 2:
-            print("expense friend")
+             get_expenses_with_a_friend(user)
         elif view_Choice == 3:
-            print("expense group")
             get_expenses_with_a_group(user)
         elif view_Choice == 4:
-            print("expense balance")
+            get_total_expenses(user)
         elif view_Choice == 5:
-            print("friend balance")
+            get_friends_with_outstanding_bal(user)
         elif view_Choice == 6:
-            print("all groups")
+            get_groups(user)
         elif view_Choice == 7:
-            print("group balance")
+            get_total_unpaid_expenses(user)
         
     elif action == 0:
-        print("You have exited Cash-App. ")
+        print("You have exited Cash-App.")
         
         
 print_msg_box(f"Welcome to Cash-App {name}!\n", indent=10)        
@@ -148,6 +183,7 @@ main_Menu()
 action_Run()
 while True:
     if action != 0:
+        print("\n----------------------------------------------------")
         main_Menu()
         action_Run()
     else:
